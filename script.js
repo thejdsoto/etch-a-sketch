c11ec044fde805c81aa9a4322f1fdb99ca3a1a40
Jade S <thedavidsoto@gmail.com>
2024-11-11 09:16:40 +0800

    Change value of cell width and height to vary depending on input size

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ const container = document.querySelector('.container');
 for (let i=0; i<16; i++) {
     let column = document.createElement('div');
     column.setAttribute("class", "column");
-    // row.style.width = "50px";
-    // row.style.height = "50px";
-    // row.style.border = "1px solid black";
     container.appendChild(column);
     for (let j=0; j<16; j++) {
         let cell = document.createElement('div');
         cell.setAttribute("class", "cell");
-        cell.style.width = "50px";
-        cell.style.height = "50px";
+        cell.style.width = `${960/dimension}`;
+        cell.style.height = `${960/dimension}`;
         cell.style.border = "1px solid black";
         column.appendChild(cell);
     }
+}
+
+const cell = document.querySelectorAll('.cell');
+
+for (let i=0; i<cell.length; i++) {
+    cell[i].addEventListener("mouseover", () => {cell[i].style.backgroundColor = "black"});
 }
